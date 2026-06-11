baeed4766ac962a09ea9ae58dcada8c616ab9356
hitsu0909 <hitsu0909@gmail.com>
2026-06-11 15:29:24 +0900

    ppp

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
+app.get('*', (req, res) => {
+  res.send('Route not found')
+})
+
 app.get('/test', (req, res) => {
   res.json({ message: 'Backend OK' })
 })
